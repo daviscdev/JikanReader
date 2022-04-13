3f6f595ccc704aa824e0267863154a47f0ff130b
rensakura <rensakura@gmail.com>
2022-04-14 00:01:04 +0800

    Implement JikanAPIService unit test

--- JikanReaderTests/JikanAPIServiceTest.swift
+++ JikanReaderTests/JikanAPIServiceTest.swift
@@ -54,4 +54,25 @@ class JikanAPIServiceTest: XCTestCase {
         
         XCTAssertEqual(result, .failure(.noResponse))
     }
+    
+    func testFetchTopAnime() {
+        let mockURLSession = MockURLSession()
+        
+        let bundle = Bundle(for: type(of: self))
+        guard let path = bundle.path(forResource: "fetchTopAnimeResponse", ofType: "txt") else {
+            XCTFail()
+            return
+        }
+        mockURLSession.data = FileManager.default.contents(atPath: path)
+        mockURLSession.error = nil
+        let service = JikanAPIService(urlSession: mockURLSession)
+        
+        var result: Result<JikanAPIGetTopAnime, JikanAPIServiceError>?
+        service.fetchTopAnime(type: nil, page: 1, completionHandler: { result = $0 })
+        
+        let data = [Anime(id: 0, url: "string", images: ["webp": CoverImage(imageURL: "string", smallImageURL: "string", largeImageURL: "string"), "jpg": CoverImage(imageURL: "string", smallImageURL: "string", largeImageURL: "string")], trailer: Trailer(youtubeID: "string", url: "string", embedURL: "string"), title: "string", titleEnglish: "string", titleJapanese: "string", titleSynonyms: ["string"], type: "TV", source: "string", episodes: 0, status: "Finished Airing", airing: true, aired: Aired(from: "string", to: "string", prop: Prop(from: From(day: 0, month: 0, year: 0), to: From(day: 0, month: 0, year: 0), string: "string")), duration: "string", rating: "G - All Ages", score: 0.0, scoredBy: 0.0, rank: 0.0, popularity: 0.0, members: 0, favorites: 0, synopsis: "string", background: "string", season: "Summer", year: 0, broadcast: Broadcast(day: "string", time: "string", timezone: "string", string: "string"), producers: [Demographic(malID: 0, type: "string", name: "string", url: "string")], licensors: [Demographic(malID: 0, type: "string", name: "string", url: "string")], studios: [Demographic(malID: 0, type: "string", name: "string", url: "string")], genres: [Demographic(malID: 0, type: "string", name: "string", url: "string")], explicitGenres: [Demographic(malID: 0, type: "string", name: "string", url: "string")], themes: [Demographic(malID: 0, type: "string", name: "string", url: "string")], demographics: [Demographic(malID: 0, type: "string", name: "string", url: "string")])]
+        let pagination = Pagination(lastVisiblePage: 0, hasNextPage: true)
+        
+        XCTAssertEqual(result, .success(JikanAPIGetTopAnime(data: data, pagination: pagination)))
+    }
 }
